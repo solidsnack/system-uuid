7c013d3e81ee9096e697ad2f902fe0236af2fc4d
Jason Dusek <jason.dusek@gmail.com>
2008-11-08 17:16:39 -0800

    Beautification.

--- System/UUID/Bytes.hs
+++ System/UUID/Bytes.hs
@@ -6,7 +6,7 @@
   #-}
 
 module System.UUID.Bytes
-  ( Bytes(..)
+  ( UUID(..)
   , runAndRead
   ) where
 
@@ -16,20 +16,23 @@ import Foreign.C
 import Foreign.ForeignPtr
 import Foreign
 import Data.Word
+import Data.List
 import Text.Printf
 
 
-data Bytes                   =  Bytes
+data UUID                    =  UUID
   !Word8 !Word8 !Word8 !Word8 !Word8 !Word8 !Word8 !Word8
   !Word8 !Word8 !Word8 !Word8 !Word8 !Word8 !Word8 !Word8
-instance Show Bytes where
-  show (Bytes x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 xA xB xC xD xE xF)
-    = concatMap (printf "%02.2x")
-      [x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, xA, xB, xC, xD, xE, xF]
-instance Eq Bytes
-instance Ord Bytes
+instance Show UUID where
+  show (UUID x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 xA xB xC xD xE xF)
+    = printf formatUUID x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 xA xB xC xD xE xF
+   where
+    formatUUID               =  intercalate "-" $ map b [ 2, 1, 1, 1, 3 ]
+    b                        =  concat . (`replicate` "%02.2x%02.2x")
+instance Eq UUID
+instance Ord UUID
 
-runAndRead                  ::  (Ptr CChar -> IO ()) -> IO Bytes 
+runAndRead                  ::  (Ptr CChar -> IO ()) -> IO UUID 
 runAndRead cProcedure        =  do
   fp                        <-  mallocForeignPtrArray 16
   withForeignPtr fp cProcedure 
@@ -37,5 +40,5 @@ runAndRead cProcedure        =  do
   let
     [x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, xA, xB, xC, xD, xE, xF]
       = bytes
-  return $ Bytes x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 xA xB xC xD xE xF
+  return $ UUID x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 xA xB xC xD xE xF
 
