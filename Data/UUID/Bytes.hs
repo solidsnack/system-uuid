164a9ed6beb156176e958d69a7b2e3f92721103f
Jason Dusek <jason.dusek@gmail.com>
2008-11-11 02:33:13 -0800

    Disinclined to integrate with the standard UUID package.

--- Data/UUID/Bytes.hs
+++ Data/UUID/Bytes.hs
@@ -9,6 +9,12 @@ module Data.UUID.Bytes
 
 
 import Data.Word
+import Data.Char
+import Control.Monad
+import Numeric
+import Text.ParserCombinators.ReadPrec (lift)
+import Text.ParserCombinators.ReadP
+import Text.Read hiding (pfail)
 import Data.List
 import Text.Printf
 
@@ -22,6 +28,24 @@ instance Show UUID where
    where
     formatUUID               =  intercalate "-" $ map b [ 2, 1, 1, 1, 3 ]
     b                        =  concat . (`replicate` "%02.2x%02.2x")
+instance Read UUID where
+  readPrec                   =  lift $ do
+    [x0,x1,x2,x3]           <-  count 4 byte
+    char '-'
+    [x4,x5]                 <-  count 2 byte
+    char '-'
+    [x6,x7]                 <-  count 2 byte
+    char '-'
+    [x8,x9]                 <-  count 2 byte
+    char '-'
+    [xA,xB,xC,xD,xE,xF]     <-  count 6 byte
+    return $ UUID x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 xA xB xC xD xE xF
+   where
+    byte                     =  do
+      s                     <-  sequence $ replicate 2 $ satisfy isHexDigit
+      case readHex s of
+        [(b, _)]            ->  return b
+        _                   ->  pfail
 instance Eq UUID
 instance Ord UUID
 
